761f19fe09e8d4a693497f99e8f12bff0dbf51ad
ErShivnandan <ec1912@global.org.in>
2024-05-23 14:19:09 +0530

    Initial commit with the project setup

---
+++ src/AllRoutes/Home/Homepage.jsx
@@ -0,0 +1,13 @@
+
+
+const Homepage = () => {
+
+    console.log("running")
+  return (
+    <div>
+      <div className="text-black text-center">Hello shiv</div>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import AllRoutes from "./AllRoutes/AllRoutes"
+
+
+const App = () => {
+  return (
+   <AllRoutes/>
+  )
+}
+
+export default App
